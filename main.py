ccfc9aebec6129cbbe12443055745e7dbca15b95
Lillian Schofield <lschofield24@biomedscienceacademy.org>
2020-11-10 23:00:39 +0000

    I changed everything. I added my requirments and fullfilled all my needs for that. I also added comments so I and the reader can read the code easier. I ran my code a plentyfull of times to make sure it works.

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Lillian Schofield
+Period/Core: 7
 
 
 """
-print("Lets Play Silly Sentances")
+print("Lets Play Silly Sentances \n")
 
-adjective = input("enter an adjective: ")
+human_name = input("Enter a human name: ")
+adjective_1 = input("Enter an adjective: ")
+dog_pronoun = input("Enter a capitilized pronoun: ")
+verb_1 = input("Enter a verb: ")
+verb_2 = input("Enter a verb: ")
+dog_name = input("Enter a dog name: ")
+city = input("Enter a city: ")
 
-print(adjective + " hello")
+#dog name
+animal = input("Enter an animal: ")
+verb_3 = input("Enter a past tense verb: ")
+
+#dog name 
+#animial 
+
+greeting = input("Enter a capitilized greeting word: ")
+#human name
+
+adjective_2 = input("Enter an adjective: ")
+
+#animal 
+
+animal_pronoun = input("Enter a pronoun: ")
+
+#human name
+
+#dog name
+
+adjective_3 = input ("Enter an adjective: ")
+
+
+#greeting to the start of the madlib
+name = input("What is your name? ")
+num = input("How many blanks will you have to fill, hint look at the comments: ")
+print(f"Hello {name}!\nYou are correct, the number is {num}!")
+
+#using the format.() function, I was able to ask the user questions to then write into my code. 
+name = input("What color is the sky? ")
+num = input("How many clouds should there be?: ")
+print(f"It was a nice and sunny day, the sky was {name}.\nThis girl and her dog were outside enjoying the weather and counting the clouds. There were {num} clouds. It was all good until something came up.\nLet's start,")
+
+
+#19 different fill-ins not all of them are different
+print(human_name + " loves to play with her " + adjective_1 + " dog. \n" + dog_pronoun + " likes to " + verb_1 + " and " + verb_2 + ". Her dog's name is " + dog_name + ". They live in " + city + ". \nOne day, " + dog_name + " got into a fight with the neighbor's " + animal + ". They " + verb_3 + " and fought. They then ran into a problem! " + dog_name + " and the " + animal + " got in trouble! \n" "\"" + greeting + ",\" " + human_name + " said. \"Oh no! What is going on here!\" \nThe big " + animal + " got up and came over. \"Nothing,\" " + animal_pronoun + " said. \n\"You two better make up!\" " + human_name + " said. \n\"Okay,\" " + dog_name + " said. They soon became " + adjective_3 + " and set their differences asside to become friends.")
